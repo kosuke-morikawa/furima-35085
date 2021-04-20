d26eeba345b080551749a9b22f4a7627c7f8d696
kosuke-morikawa <haoyousenchuan@gmail.com>
2021-04-20 16:26:03 +0900

    ユーザー管理機能　修正４

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  
-  def show
-    @user = User.find(params[:id])
-  end
-end
+#class UsersController < ApplicationController
+  #before_action :authenticate_user!, except: :index
+
+  #def show
+   #@user = User.find(params[:id])
+  #end
+#end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,13 @@ class User < ApplicationRecord
 
 
   with_options presence: true do
+    validates :nickname
     # 英数字混合にする
     validates :password,format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Inputhalf-width sharacters."}
-   
-    validates :nickname
     # ユーザーのフリガナは全角のカタカナにする。（苗字）
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/}
     # ユーザーのフリガナは全角のカタカナにする。（姓名）
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/}
-
     validates :last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :birth_date
