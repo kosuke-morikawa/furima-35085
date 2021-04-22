c7b92a91bf1ada7b422065e39c83c67638c7de84
kosuke-morikawa <haoyousenchuan@gmail.com>
2021-04-22 17:31:29 +0900

    商品出品機能　修正３

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!,only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!,except: [:index, :show ]
   def index
     # @products = Product.order('created_at DESC')
   end
