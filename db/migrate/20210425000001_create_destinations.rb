78203a5f2890d832e75406081e73fbf907b03d37
kosuke-morikawa <haoyousenchuan@gmail.com>
2021-04-27 12:04:49 +0900

    アクティブハッシュ　修正

--- db/migrate/20210425000001_create_destinations.rb
+++ db/migrate/20210425000001_create_destinations.rb
@@ -3,7 +3,7 @@ class CreateDestinations < ActiveRecord::Migration[6.0]
     create_table :destinations do |t|
       t.references :order,             null: false, foreign_key: true
       t.string     :postal_code,       null: false
-      t.string     :prefecture_id,     null: false
+      t.integer    :prefecture_id,     null: false
       t.string     :city,              null: false
       t.string     :addresses,         null: false
       t.string     :building
